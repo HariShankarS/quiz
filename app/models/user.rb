e8df508536cbce5e0ac8d0a1061c39c252de642e
HariShankarSrungavarapu <s.harishankar333@gmail.com>
2017-02-06 17:06:28 +0530

    Added validation method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,20 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :email, :inclusion => { :in => RegisteredUser.all.collect(&:email), :message => " is not registered with us" }
+  # validates :email, :inclusion => { :in => RegisteredUser.all.collect(&:email), :message => " is not registered with us" }
+  validate :email_registered_or_not
   has_many :attempts, dependent: :destroy
   has_many :evaluations, :through => :attempts, dependent: :destroy
 
   def attempted_evaluations
   	attempts.pluck(:evaluation_id, :unfinished).map do |arr| Hash.send(:[], *arr) end.reduce({}, :merge)
+  end
+
+  def email_registered_or_not
+    if RegisteredUser.present?
+      unless RegisteredUser.all.collect(&:email).include?(email)
+        errors.add(:email,"is not registered with us")
+      end
+    end
   end
 end
